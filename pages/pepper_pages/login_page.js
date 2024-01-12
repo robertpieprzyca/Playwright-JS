109b42e84c5cc02c0e4c468a80afd5ffe98a0346
robertpieprzyca <119869915+robertpieprzyca@users.noreply.github.com>
2024-01-12 21:57:49 +0100

    Revert "Adding comment"

--- pages/pepper_pages/login_page.js
+++ pages/pepper_pages/login_page.js
@@ -10,7 +10,6 @@ exports.LoginPage = class LoginPage {
     this.user_details = ".flex.flex--1.boxAlign-jc--all-c";
   }
 
-  // login
   async goto_login_page() {
     await this.page.goto("https://www.pepper.pl/dlaciebie");
   }
