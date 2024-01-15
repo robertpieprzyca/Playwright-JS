679d607c17b57daaa998a03e1d78a71004cfb073
robert <robpieprzyca@gmail.com>
2024-01-15 10:15:40 +0100

    zmiana komentarza w login_page

--- pages/pepper_pages/login_page.js
+++ pages/pepper_pages/login_page.js
@@ -10,7 +10,7 @@ exports.LoginPage = class LoginPage {
     this.user_details = ".flex.flex--1.boxAlign-jc--all-c";
   }
 
-  //login
+  //login page
   async goto_login_page() {
     await this.page.goto("https://www.pepper.pl/dlaciebie");
   }
